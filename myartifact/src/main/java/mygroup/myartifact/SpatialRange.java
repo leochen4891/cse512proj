b541ed9295e069aaf43e58ff3fa99e5af85b22c8
Lei Chen <leochen4891@gmail.com>
2015-10-03 20:36:28 -0700

    Spatial Range 1st version finished

--- myartifact/src/main/java/mygroup/myartifact/SpatialRange.java
+++ myartifact/src/main/java/mygroup/myartifact/SpatialRange.java
@@ -13,18 +13,19 @@ import org.apache.hadoop.util.Progressable;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
-import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.JavaRDD;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
 
-public class GeometricUnion {
+public class SpatialRange {
 	private static final String HDFS_ROOT_PATH = "hdfs://192.168.184.165:54310/";
 	private static final String LOCAL_PATH = "/home/user/";
 
-	private static final String INPUT_FILE = "union_input.csv";
-	private static final String OUTPUT_FILE = "union_output.csv";
+	private static final String INPUT_FILE_1 = "range_input_1.csv";
+	// TODO read input 2 from file
+	// private static final String INPUT_FILE = "range_input.csv";
+	private static final String OUTPUT_FILE = "range_output.csv";
 
 	public static void main(String[] args) {
 		JavaSparkContext sc = null;
@@ -32,33 +33,36 @@ public class GeometricUnion {
 		OutputStream outputStream = null;
 
 		try {
-			System.out.println("Geometric Union Start!");
+			System.out.println("Spatial Range Query Start!");
 
-			SparkConf conf = new SparkConf().setAppName("MyAppName").setMaster("local");
-			sc = new JavaSparkContext(conf);
 			// 0. copy local file to HDFS
-			HDFSFileCopy.copyToHDFS(INPUT_FILE, INPUT_FILE, HDFS_ROOT_PATH, LOCAL_PATH);
+			HDFSFileCopy.copyToHDFS(INPUT_FILE_1, INPUT_FILE_1, HDFS_ROOT_PATH, LOCAL_PATH);
 
 			// 1. read the lines from the input file in HDFS
-			JavaRDD<String> lines = sc.textFile(HDFS_ROOT_PATH + INPUT_FILE);
+			SparkConf conf = new SparkConf().setAppName("MyAppName").setMaster("local");
+			sc = new JavaSparkContext(conf);
+			JavaRDD<String> lines = sc.textFile(HDFS_ROOT_PATH + INPUT_FILE_1);
+			// TODO read from input_file_2 instead of hard coded
+			final Geometry window = UnionJTS.getRectangleFromLeftTopAndRightBottom(0, 0, 1, 1);
 
-			// 2. Geometric Union
+			// 2. spatial range query
 			// 2.1 map: convert each line of string to a polygon
 			JavaRDD<Geometry> polygons = lines.map(new Function<String, Geometry>() {
-				private static final long serialVersionUID = -1928298089452870258L;
+				private static final long serialVersionUID = -4119796271168086532L;
 
 				public Geometry call(String s) {
 					return UnionJTS.getRectangleFromLeftTopAndRightBottom(s);
 				}
 			});
 
-			// 2.2 reduce: combine every 2 polygons
-			Geometry finalPolygon = polygons.reduce(new Function2<Geometry, Geometry, Geometry>() {
-				private static final long serialVersionUID = -1967342595615519573L;
+			// 2.2 filter: filter to allow the ones only in the query window
+			JavaRDD<Geometry> rangeResults = polygons.filter(new Function<Geometry, Boolean>() {
+				private static final long serialVersionUID = -5424930742935658378L;
 
 				@Override
-				public Geometry call(Geometry arg0, Geometry arg1) throws Exception {
-					return arg0.union(arg1);
+				public Boolean call(Geometry g) throws Exception {
+					// return broadcastWindow.getValue().contains(g);
+					return window.contains(g);
 				}
 			});
 
@@ -71,14 +75,17 @@ public class GeometricUnion {
 				}
 			});
 			PrintWriter pw = new PrintWriter(outputStream);
-			for (Coordinate cor : finalPolygon.getCoordinates()) {
-				pw.println(cor.x + ", " + cor.y);
+			for (Geometry g : rangeResults.collect()) {
+				for (Coordinate cor : g.getCoordinates()) {
+					pw.println(cor.x + ", " + cor.y);
+				}
 			}
 			pw.flush();
 			pw.close();
 
 			// 4. copy the output file from HDFS to local disk
 			HDFSFileCopy.copyFromHDFS(OUTPUT_FILE, OUTPUT_FILE, HDFS_ROOT_PATH, LOCAL_PATH);
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -88,5 +95,4 @@ public class GeometricUnion {
 				sc.close();
 		}
 	}
-
 }
